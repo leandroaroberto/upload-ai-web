93ac76d781ab6d89e5ac2c37c40e5fc83a0a9271
Leandro Aparecido Roberto <leandro.roberto@ciandt.com>
2023-09-12 22:32:45 -0300

    Left side layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
+import { Button } from "./components/ui/button";
+import { Github } from "lucide-react";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
+
 export function App() {
 
   return (
-    <>
-      <h1>Hello AI</h1>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex justify-between items-center border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Developed with love in NLW</span>
+          <Separator orientation="vertical" className="h-6"/>
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2"/>
+            Github
+          </Button>
+        </div>
+      </div>
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Include a prompt for the AI..."
+            />
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="AI generated result..."
+              readOnly
+            />
+          </div>
+          <p className="text-sm text-muted-foreground">
+            Remember you can use the <code className="text-violet-400">{'{transcription}'}</code> variable in your prompt to add the selected video transcription content.
+          </p>
+        </div>
+        <aside className="w-80 bg-green-300">
+
+        </aside>
+      </main>
+    </div>
   )
 }
 
