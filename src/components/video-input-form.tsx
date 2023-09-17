82ef71220b809b324627bf7f074b92cc49c36d93
Leandro Aparecido Roberto <leandro.roberto@ciandt.com>
2023-09-17 17:55:20 -0300

    Video to mp3 converter using ffmpeg wasm

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -3,10 +3,13 @@ import { Separator } from "./ui/separator"
 import { Label } from "./ui/label"
 import { Textarea } from "./ui/textarea"
 import { Button } from "./ui/button"
-import { ChangeEvent, useMemo, useState } from "react"
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react"
+import { getFFmpeg } from "@/lib/ffmpeg"
+import { fetchFile } from "@ffmpeg/util"
 
 export const VideoInputForm = () => {
   const [ videoFile, setVideoFile ] = useState<File | null>(null)
+  const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
   function handleFileSelected(event : ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget
@@ -26,8 +29,58 @@ export const VideoInputForm = () => {
     return URL.createObjectURL(videoFile)
   }, [videoFile])
 
+  async function handleUploadVideo(event : FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const prompt = promptInputRef.current?.value
+
+    if (! videoFile) {
+      return
+    }
+
+    const audioFile = await convertVideoToAudio(videoFile)
+    console.log(audioFile)
+  }
+
+  async function convertVideoToAudio(video : File) {
+    console.log('Convert started.')
+
+    const ffmpeg = await getFFmpeg()
+    await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+
+    // ffmpeg.on('log', log => {
+    //   console.log(log)
+    // })
+
+    ffmpeg.on('progress', progress => {
+      console.log('Convert process: ' + Math.round(progress.progress * 100))
+    })
+
+    await ffmpeg.exec([
+      '-i',
+      'input.mp4',
+      '-map',
+      '0:a',
+      '-b:a',
+      '20k',
+      '-acodec',
+      'libmp3lame',
+      'output.mp3'
+    ])
+
+    const data = await ffmpeg.readFile('output.mp3')
+    const audioFileBlob = new Blob([data], {type: 'audio/mpeg'})
+    const audioFile = new File([audioFileBlob], 'audio.mp3', {
+      type: 'audio/mpeg'
+    })
+
+    console.log('Convert finished')
+
+    return audioFile
+
+  }
+
   return (
-    <form className="space-y-6">
+    <form className="space-y-6" onSubmit={handleUploadVideo}>
             <label
               htmlFor="video"
               className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
@@ -47,6 +100,7 @@ export const VideoInputForm = () => {
             <div className="space-y-2">
               <Label htmlFor="transcription_prompt">Transcription prompt</Label>
               <Textarea
+                ref={promptInputRef}
                 id="transcription_prompt"
                 className="h-20 leading-relaxed resize-none"
                 placeholder="include key words mentioned in the video separeted by comma (,)"
